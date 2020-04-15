3746f2a47b5eddf68deb2f90efa989428aedd95e
Daniel Foad <daniel.foad@sky.uk>
2020-04-15 16:42:50 +0100

    begone electron-updater

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const { app, BrowserWindow } = require('electron');
-const { autoUpdater } = require('electron-updater');
-
 
 function initialiseApp() {
-  autoUpdater.checkForUpdatesAndNotify();
   createWindow();
 }
 
@@ -21,42 +18,6 @@ function createWindow() {
 
   // mainWindow.webContents.openDevTools();
 }
-
-let updateWindow;
-
-function createUpdateWindow() {
-  updateWindow = new BrowserWindow();
-  updateWindow.on('closed', () => { updateWindow = null });
-
-  updateWindow.loadFile('update.html');
-  return updateWindow;
-}
-
-function sendStatusToWindow(text) {
-  log.info(text);
-  updateWindow.webContents.send('message', text);
-}
-
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available, downloading...');
-  updateWindow.webContents.send('version', app.getVersion());
-});
-
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Failed to download update: ' + err);
-});
-
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-});
-
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded, please restart to get new version');
-});
-
 app.whenReady().then(initialiseApp);
 
 app.on('window-all-closed', function () {
